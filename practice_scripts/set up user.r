f8509a3a9c63c8e5eec8a83c590b8bd6f573632a
SWRUG <simon.wellesley-miller@nhs.net>
2022-11-23 13:14:26 +0000

    update

---
+++ practice_scripts/set up user.r
@@ -0,0 +1,5 @@
+library(usethis)
+use_git_config(
+  user.name = "SWRUG",          # actual first and last name
+  user.email = "simon.wellesley-miller@nhs.net"  # email associated with GitHub account
+)
